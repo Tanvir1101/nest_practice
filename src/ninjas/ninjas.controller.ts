51c4511935b0a9fd921bc0ecf3d10f7a13c465cd
tanvirsuperanova <tanvir@supernovatechbd.com>
2024-10-06 18:09:58 +0600

    first commit net ninja

---
+++ src/ninjas/ninjas.controller.ts
@@ -0,0 +1,10 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('ninjas')
+export class NinjasController {
+    // GET /ninjas --> []
+    // GET /ninjas/:id --> {...}
+    // POST /ninjas
+    // PUT /ninjas/:id --> {...}
+    // DELETE /ninjas/:id
+}
