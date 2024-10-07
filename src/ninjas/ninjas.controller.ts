68112d9129b392fb81ff9703508fa1ded9d083fd
tanvirsuperanova <tanvir@supernovatechbd.com>
2024-10-07 17:53:21 +0600

    providers

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,10 +1,43 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+
 
 @Controller('ninjas')
 export class NinjasController {
     // GET /ninjas --> []
+    @Get()
+  getNinjas() {
+        return [];
+    }
+
     // GET /ninjas/:id --> {...}
+    @Get(':id')
+    getOneNinja(@Param('id') id: string) {
+        return {
+            id,
+        };
+    }
+
     // POST /ninjas
+    @Post()
+    createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+        return {
+            name: createNinjaDto.name
+        };
+    }
+
     // PUT /ninjas/:id --> {...}
+    @Put(':id')
+    updateNinja(@Param('id') id: string, @Body() updateNinjaDto: CreateNinjaDto) {
+        return {
+            id,
+            name: updateNinjaDto.name
+        };
+    }
+
     // DELETE /ninjas/:id
+    @Delete(':id')
+    removeNinja() {
+        return {};
+    }
 }
